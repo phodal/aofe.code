2143ac77de8531bcecf0135a6396558d8edc4dce
Phodal HUANG <h@phodal.com>
2018-09-05 13:18:27 +0800

    [T] fix typo

--- chapter02/gulp-examples/gulpfile.js
+++ chapter02/gulp-examples/gulpfile.js
@@ -6,20 +6,20 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('html', function(){
-  return gulp.src('client/templates/*.pug')
+  return gulp.src('src/templates/*.pug')
     .pipe(pug())
     .pipe(gulp.dest('build/html'))
 });
 
 gulp.task('css', function(){
-  return gulp.src('client/css/*.less')
+  return gulp.src('src/css/*.less')
     .pipe(less())
     .pipe(minifyCSS())
     .pipe(gulp.dest('build/css'))
 });
 
 gulp.task('js', function(){
-  return gulp.src('client/javascript/*.js')
+  return gulp.src('src/javascript/*.js')
     .pipe(sourcemaps.init())
     .pipe(concat('app.min.js'))
     .pipe(sourcemaps.write())
