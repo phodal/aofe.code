ffad0a1cefa30cdec4c51329e2dd0df6dfa4aade
Phodal HUANG <h@phodal.com>
2018-09-11 12:44:57 +0800

    [T] add koa examples

--- chapter03/server-side-example/app.js
+++
@@ -1,8 +0,0 @@
-const Koa = require('koa');
-const app = module.exports = new Koa();
-
-app.use(async function(ctx) {
-  ctx.body = 'Hello World';
-});
-
-if (!module.parent) app.listen(3000);
---
+++ chapter03/server-side-example/src/app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const views = require('koa-views');
+const Koa = require('koa');
+const app = module.exports = new Koa();
+
+app.use(views(path.join(__dirname, '/views'), { extension: 'ejs' }));
+
+// dummy data
+
+const user = {
+  name: {
+    first: 'Tobi',
+    last: 'Holowaychuk'
+  },
+  species: 'ferret',
+  age: 3
+};
+
+// render
+
+app.use(async function(ctx) {
+  await ctx.render('user', { user });
+});
+
+if (!module.parent) app.listen(3000);
