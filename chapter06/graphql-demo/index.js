a14017d65ee94ef7a52704f8023520453e2e8737
Phodal HUANG <h@phodal.com>
2018-11-07 13:06:50 +0800

    [ch6] fix: remove some comments

--- chapter06/graphql-demo/index.js
+++ chapter06/graphql-demo/index.js
@@ -1,38 +1,19 @@
 const {ApolloServer, gql} = require('apollo-server');
 const {RESTDataSource} = require('apollo-datasource-rest');
 
-const makes = [
-  {
-    "id": 1,
-    "category": 1,
-    "featured_image": "uploads/make/.thumbnails/amazon-echo.jpg/amazon-echo-600x360.jpg"
-  },
-  {
-    "id": 2,
-    "category": 2,
-    "featured_image": "uploads/make/.thumbnails/connected-smart-cities.jpg/connected-smart-cities-600x360.jpg"
-  }];
-
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
   type Make {
     id: ID!
     category: String
     featured_image: String
   }
 
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
   type Query {
     make(id: ID!): Make
     makes(limit: Int): [Make]
   }
 `;
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve makes from the "makes" array above.
 const resolvers = {
   Query: {
     make: async (_source, { id }, { dataSources }) => {
@@ -60,9 +41,6 @@ class MakeAPI extends RESTDataSource {
   }
 }
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -71,8 +49,6 @@ const server = new ApolloServer({
   }),
 });
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
 server.listen().then(({url}) => {
   console.log(`🚀  Server ready at ${url}`);
 });
