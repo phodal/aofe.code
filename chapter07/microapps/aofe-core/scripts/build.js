b6e23d224ef3daaf3ca83b43bd1d96a13661ef33
Phodal HUANG <h@phodal.com>
2018-11-19 22:51:03 +0800

    [ch07] core add build scripts

---
+++ chapter07/microapps/aofe-core/scripts/build.js
@@ -0,0 +1,11 @@
+let fs = require('fs-extra')
+let projects = ['reports', 'settings', 'dashboard'];
+
+for(let index in projects) {
+  let project = projects[index];
+  let originModulePath = './src/app/' + project;
+  fs.removeSync(originModulePath);
+  fs.copy('../' + project + '/src/app/' + project, originModulePath)
+    .then(() => console.log('copy project: ' + project + ' success!'))
+    .catch(err => console.error(err));
+}
